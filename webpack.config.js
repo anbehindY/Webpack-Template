f58bf842318c7a5ed06ee334e149cda806b0d9c3
anbehindY <yanminthwin@gmail.com>
2023-03-28 18:03:17 +0630

    Add CSS and icon to output

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,20 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
